e001e30da89fabff83729afe4ea54362e66620b3
mdmarufhossianbd <mdmarufh974@gmail.com>
2024-09-15 13:57:25 +0600

    category pagination complate

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -17,7 +17,7 @@ const CategoryJobsWithPagination = ({ slug }) => {
         const fetchCategoryJobs = async (slug, page) => {
             setLoading(true)
             const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
-            await axios.get(`${baseUrl}/api/v1/jobs/category-wise-jobs/${slug}?${page}&limit=2`)
+            await axios.get(`${baseUrl}/api/v1/jobs/category-wise-jobs/${slug}?page=${page}&limit=10`)
             .then(res => {                
                 if(res.data.success){
                     setJobs(res.data?.result)
@@ -35,7 +35,9 @@ const CategoryJobsWithPagination = ({ slug }) => {
     const handleNext = () =>{
         setPage(page + 1)
     }
-
+    const handlePrevius = () => {
+        setPage(page - 1)
+    }
     return (
         <div>
             {
@@ -44,8 +46,8 @@ const CategoryJobsWithPagination = ({ slug }) => {
             <div className="my-10 flex items-center justify-between gap-5">
                 <p>{page} of {totalPages} Pages</p>
                 <div className="flex items-center gap-3">
-                    <button  className="flex gap-1 px-5 py-2 bg-[#f6f8fc] hover:bg-[#1e508c] hover:text-white duration-500 rounded-md disabled:cursor-not-allowed"><IconArrowNarrowLeft stroke={2} />Previous</button>
-                    <button onClick={handleNext} className="flex gap-1 px-5 py-2 bg-[#f6f8fc] hover:bg-[#1e508c] hover:text-white duration-500 rounded-md disabled:cursor-not-allowed">Next<IconArrowNarrowRight stroke={2} /></button>
+                    <button onClick={handlePrevius} disabled={page === 1} className="flex gap-1 px-5 py-2 bg-[#1e508c] hover:bg-[#184174] text-white duration-500 rounded-md disabled:cursor-not-allowed disabled:bg-[#f6f8fc] disabled:text-black"><IconArrowNarrowLeft stroke={2} />Previous</button>
+                    <button onClick={handleNext} disabled={page === totalPages} className='flex gap-1 px-5 py-2 bg-[#1e508c] hover:bg-[#184174] text-white duration-500 rounded-md disabled:cursor-not-allowed disabled:bg-[#f6f8fc] disabled:text-black'>Next<IconArrowNarrowRight stroke={2} /></button>
                 </div>
             </div>
         </div>
